867b63f68efd753ab17397428f29c4dd8bb82916
Alexandr Naum <mfq07k9cz@gmail.com>
2022-04-22 13:03:12 +0300

    services-inner page

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,3 +1,4 @@
 console.log('components');
 
 import './components/sliders';
+import './components/accordion';
---
+++ src/js/components/accordion.js
@@ -0,0 +1,15 @@
+import GraphAccordion from '../vendor/graph-accordion';
+
+if (document.querySelector('.we-offer')) {
+  const accordion1 = new GraphAccordion('.accordion-1', {
+    speed: 300
+  });
+
+  const accordion2 = new GraphAccordion('.accordion-2', {
+    speed: 300
+  });
+}
+
+
+
+
